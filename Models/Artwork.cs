defa490a950c12282ffa17fadc33570d87a028d9
GeorgeShani <shanidze.geo2004@gmail.com>
2025-04-26 12:01:13 +0400

    Update Artwork properties

--- Models/Artwork.cs
+++ Models/Artwork.cs
@@ -2,5 +2,17 @@
 
 public class Artwork
 {
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string ImageUrl { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     
+    public int PortfolioId { get; set; }
+    public Portfolio? Portfolio { get; set; }
+    
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    
+    public ICollection<Critique>? Critiques { get; set; }
 }
