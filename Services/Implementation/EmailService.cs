675f8b43c7547ca1f35d8f44488912d7759de8ee
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-03 10:06:08 +0400

    Add SMTP Display Name for the SMTP Service

--- Services/Implementation/EmailService.cs
+++ Services/Implementation/EmailService.cs
@@ -6,10 +6,11 @@ namespace ArtUnion_API.Services.Implementation;
 
 public class EmailService : IEmailService
 {
-    private readonly string smtpHost = Environment.GetEnvironmentVariable("SMTP_HOST")!;
     private readonly int smtpPort = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT")!);
+    private readonly string smtpHost = Environment.GetEnvironmentVariable("SMTP_HOST")!;
     private readonly string smtpUsername = Environment.GetEnvironmentVariable("SMTP_USERNAME")!;
     private readonly string smtpPassword = Environment.GetEnvironmentVariable("SMTP_PASSWORD")!;
+    private readonly string smtpDisplayName = Environment.GetEnvironmentVariable("SMTP_DISPLAY_NAME")!;
     
     private readonly SmtpClient _smtpClient;
 
@@ -26,7 +27,7 @@ public class EmailService : IEmailService
     public async Task SendEmailAsync(string to, string subject, string message)
     {
         using var mailMessage = new MailMessage();
-        mailMessage.From = new MailAddress(smtpUsername);
+        mailMessage.From = new MailAddress(smtpUsername, smtpDisplayName);
         mailMessage.Subject = subject;
         mailMessage.Body = message;
         mailMessage.IsBodyHtml = true;
