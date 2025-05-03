2418a4eee25f02f6992c2ed4a7f083857de3eabe
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-03 10:30:29 +0400

    Initialize Other Services

---
+++ Services/Implementation/UserService.cs
@@ -0,0 +1,8 @@
+﻿using ArtUnion_API.Services.Interfaces;
+
+namespace ArtUnion_API.Services.Implementation;
+
+public class UserService : IUserService
+{
+    
+}
---
+++ Services/Interfaces/IArtworkService.cs
@@ -0,0 +1,6 @@
+﻿namespace ArtUnion_API.Services.Interfaces;
+
+public interface IArtworkService
+{
+    
+}
