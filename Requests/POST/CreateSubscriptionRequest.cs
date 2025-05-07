da54185d3bafed3168e9cee60d918d902591d20a
GeorgeShani <shanidze.geo2004@gmail.com>
2025-05-07 16:02:18 +0400

    Create POST Request Model and Validator for Subscription

---
+++ Requests/POST/CreateSubscriptionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ArtUnion_API.Requests.POST;
+
+public class CreateSubscriptionRequest
+{
+    public int SubscriberId { get; set; }
+    public int ArtistId { get; set; }
+    public DateTime SubscribedAt { get; set; } = DateTime.UtcNow;
+}
